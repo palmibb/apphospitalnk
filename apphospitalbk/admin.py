e90158930cd712412925bfd0a7486d64440e82f3
DESKTOP-VK2K1Q1\Carlos <cpalmi0725@gmail.com>
2022-09-23 13:23:45 -0500

    procfile update/settings.py update

---
+++ apphospitalbk/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import User
+from .models import Account
+
+admin.site.register(User)
+admin.site.register(Account)
+# Register your models here.
