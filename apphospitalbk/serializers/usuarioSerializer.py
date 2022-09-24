5427fe9216a10ec664e7c5621b5dab4bbc299631
AdrianAcostaG <adrianacostag@gmail.com>
2022-09-24 18:27:41 -0500

    serializadoresObjetoNuevo

--- apphospitalbk/serializers/usuarioSerializer.py
+++ apphospitalbk/serializers/usuarioSerializer.py
@@ -1,3 +1,4 @@
+from apphospitalnk.apphospitalbk.models import usuario
 from rest_framework import serializers
 from apphospitalbk.models.usuario import Usuario
 
@@ -7,3 +8,16 @@ class UsuarioSerializer(serializers.ModelSerializer):
         model = Usuario
         fields = ['id', 'username', 'password', 'name', 'email',]
  
+    def create (self, validated_data):
+        useruarioInstance = Usuario.objects.create(**validated_data)
+        return useruarioInstance
+
+    def to_representation(self, obj):
+        usuario = Usuario.objects.get(id=obj.id)
+        return {
+            'id': usuario.id,
+            'username':usuario.username, 
+            'password': usuario.password, 
+            'name': usuario.name, 
+            'email': usuario.email,
+        }
