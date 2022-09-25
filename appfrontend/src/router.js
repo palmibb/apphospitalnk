fc043b9f1b64feb3aa369ba905cf952deefa3117
vadelgado <vadelgado@udenar.edu.co>
2022-09-24 19:24:02 -0500

    login y autenticacion

--- appfrontend/src/router.js
+++ appfrontend/src/router.js
@@ -1,12 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-
+import LogIn from './components/LogIn.vue'
+import Home from'./components/Inicio.vue'
+import SignUp from'./components/Registrarse.vue'
 const routes = [
   {
     path: '/',
     name: 'root',
     component: App
-  }
+  },
+  {
+    path: '/usuario/inicio',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/usuario/ingreso',
+    name: 'logIn',
+    component: LogIn
+  },
+  {
+    path: '/usuario/registrar',
+    name: 'signUp',
+    component: SignUp
+  },
 ]
 
 const router = createRouter({
